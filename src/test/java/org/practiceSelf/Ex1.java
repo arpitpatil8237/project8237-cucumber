7def7d7aa6b2e3949f3442c4fd70a0e156b8f199
arpitpatil8237 <rahulsonkar2795@gmail.com>
2024-01-03 17:53:10 +0530

    ChangesRemoved

--- src/test/java/org/practiceSelf/Ex1.java
+++ src/test/java/org/practiceSelf/Ex1.java
@@ -7,7 +7,7 @@ import org.utilities.BaseUtility;
 
 public class Ex1 {
 public static void main(String[] args) {
-//Branch A
+//Removed
 	BaseUtility bu=new BaseUtility();
 	WebDriver driver;
 //	WebDriver driver=new EdgeDriver();
